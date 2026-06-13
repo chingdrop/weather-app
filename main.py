c9f90fd0e704c7684ae8779843aae98fdd3d4f0a
Craig <chingdorp@pm.me>
2026-06-12 20:51:13 -0400

    create flask app from current function

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import openmeteo_requests
-
 import pandas as pd
 import requests_cache
+from flask import Flask, jsonify
 from retry_requests import retry
+
+app = Flask(__name__)
 
 
 def get_weather_data():
-    # Setup the Open-Meteo API client with cache and retry on error
     cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
     retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
     openmeteo = openmeteo_requests.Client(session=retry_session)
 
-    # Make sure all required weather variables are listed here
-    # The order of variables in hourly or daily is important to assign them correctly below
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
         "latitude": 27.0442,
@@ -25,38 +24,38 @@ def get_weather_data():
         "precipitation_unit": "inch",
     }
     responses = openmeteo.weather_api(url, params=params)
-
-    # Process first location. Add a for-loop for multiple locations or weather models
     response = responses[0]
-    print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
-    print(f"Elevation: {response.Elevation()} m asl")
-    print(f"Timezone: {response.Timezone()}{response.TimezoneAbbreviation()}")
-    print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
 
-    # Process hourly data. The order of variables needs to be the same as requested.
     hourly = response.Hourly()
-    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-    hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
-    hourly_precipitation_probability = hourly.Variables(2).ValuesAsNumpy()
-    hourly_wind_speed_10m = hourly.Variables(3).ValuesAsNumpy()
-    hourly_wind_direction_10m = hourly.Variables(4).ValuesAsNumpy()
+    hourly_data = {
+        "date": pd.date_range(
+            start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+            end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+            freq=pd.Timedelta(seconds=hourly.Interval()),
+            inclusive="left"
+        ).tz_convert(response.Timezone().decode()).strftime("%Y-%m-%dT%H:%M:%S%z").tolist(),
+        "temperature_2m": hourly.Variables(0).ValuesAsNumpy().tolist(),
+        "relative_humidity_2m": hourly.Variables(1).ValuesAsNumpy().tolist(),
+        "precipitation_probability": hourly.Variables(2).ValuesAsNumpy().tolist(),
+        "wind_speed_10m": hourly.Variables(3).ValuesAsNumpy().tolist(),
+        "wind_direction_10m": hourly.Variables(4).ValuesAsNumpy().tolist(),
+    }
 
-    hourly_data = {"date": pd.date_range(
-        start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
-        end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
-        freq=pd.Timedelta(seconds=hourly.Interval()),
-        inclusive="left"
-    ).tz_convert(response.Timezone().decode()), "temperature_2m": hourly_temperature_2m,
-                   "relative_humidity_2m": hourly_relative_humidity_2m,
-                   "precipitation_probability": hourly_precipitation_probability,
-                   "wind_speed_10m": hourly_wind_speed_10m, "wind_direction_10m": hourly_wind_direction_10m}
-
-    hourly_dataframe = pd.DataFrame(data=hourly_data)
-    print("\nHourly data\n", hourly_dataframe)
+    return {
+        "meta": {
+            "latitude": response.Latitude(),
+            "longitude": response.Longitude(),
+            "elevation": response.Elevation(),
+            "timezone": response.Timezone().decode(),
+        },
+        "hourly": hourly_data,
+    }
 
 
-# Press the green button in the gutter to run the script.
+@app.route("/weather")
+def weather():
+    return jsonify(get_weather_data())
+
+
 if __name__ == '__main__':
-    get_weather_data()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
